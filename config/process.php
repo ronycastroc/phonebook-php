2ddecd66e4d95b12d13b650d595d61b0948f557e
ronycastroc <castroroony@gmail.com>
2023-09-04 20:31:39 -0300

    feat: show a contact

--- config/process.php
+++ config/process.php
@@ -5,14 +5,36 @@
   include_once("connection.php");
   include_once("url.php");
 
-  $contacts = [];
+  if(!empty($_GET)) {
+    $id = $_GET['id'];
+  }
 
-  $query = "SELECT * FROM contacts";
 
-  $stmt = $conn->prepare($query);
+  if(!empty($id)) {
+    // GET ONE CONTACT
 
-  $stmt->execute();
+    $query = "SELECT * FROM contacts WHERE id = :id";
 
-  $contacts = $stmt->fetchAll();
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":id", $id);
+
+    $stmt->execute();
+
+    $contact = $stmt->fetch();
+
+  } else {
+    // GET ALL CONTACTS
+    $contacts = [];
+
+    $query = "SELECT * FROM contacts";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->execute();
+
+    $contacts = $stmt->fetchAll();
+
+}
 
 ?>
