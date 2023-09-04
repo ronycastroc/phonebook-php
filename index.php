a505e0e3cbb3bbbcd620a6dd0f53637585618d35
ronycastroc <castroroony@gmail.com>
2023-09-04 13:28:50 -0300

    feat: getting contacts from db in home page

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php
   include_once("templates/header.php");
 ?>
-  <h1>Init Config</h1>
-  <i class="fas fa-eye"></i>
+  <div class="container">
+    <?php if(isset($printMsg) && $printMsg != ''): ?>
+      <p id="msg"><?= $printMsg ?></p>
+    <?php endif; ?>
+    <h1 id="main-title">My Phonebook</h1>
+    <?php if(count($contacts) > 0): ?>
+      <table class="table" id="contacts-table">
+        <thead>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Phone</th>
+          <th scope="col"></th>
+        </thead>
+        <tbody>
+          <?php foreach($contacts as $contact): ?>
+            <tr>
+              <td scope="row"><?= $contact["id"] ?></td>
+              <td scope="row"><?= $contact["name"] ?></td>
+              <td scope="row"><?= $contact["phone"] ?></td>
+              <td class="actions">
+                <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                <a href="#"><i class="far fa-edit edit-icon"></i></a>
+                <button type="submit"><i class="fas fa-times deleteicon"></i></button>
+              </td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    <?php else: ?>
+      <p id="empty-list-text">There are no contacts in your phonebook yet, <a href="<?= $BASE_URL ?>/create.php">Click here to add</a>.</p>
+    <?php endif; ?>
+  </div>
 <?php
   include_once("templates/footer.php");
 ?>
