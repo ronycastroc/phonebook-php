34f4ad41f1d16b00cc9cdf5fd27174890300f677
ronycastroc <castroroony@gmail.com>
2023-09-04 12:57:14 -0300

    feat: process file

--- config/connection.php
+++ config/connection.php
@@ -1,4 +1,5 @@
 <?php
+
   $host = "localhost";
   $dbname = "phonebook";
   $user = "root";
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,13 @@
 <?php
+
   include_once("config/url.php");
-  include_once("config/connection.php");
+  include_once("config/process.php");
+
+  if(isset($_SESSION['msg'])) {
+    $printMsg = $_SESSION['msg'];
+    $_SESSION['msg'] = '';
+  }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
